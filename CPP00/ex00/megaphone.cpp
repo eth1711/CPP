43df1d23047596188bd2abff03b044150d6a1c4d
eth1711 <ethanlimwl@gmail.com>
2024-01-11 19:54:43 +0800

    first commit

---
+++ CPP00/ex00/megaphone.cpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: etlim <etlim@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/11 14:57:24 by etlim             #+#    #+#             */
+/*   Updated: 2024/01/11 19:10:00 by etlim            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main(int ac, char** argv)
+{
+    if (ac < 1)
+    {
+        cout << "*LOUD AND UNBEARABLE FEEDBACK NOISE*" << endl;
+    }
+}
