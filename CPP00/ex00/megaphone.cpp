0ed070a495437139571c081a61f9a62f8b7826c8
eth1711 <ethanlimwl@gmail.com>
2024-01-17 15:03:31 +0800

    commit before adding stuff to ex01

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: etlim <etlim@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 14:57:24 by etlim             #+#    #+#             */
-/*   Updated: 2024/01/11 19:10:00 by etlim            ###   ########.fr       */
+/*   Updated: 2024/01/15 18:12:49 by etlim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,27 @@ using std::endl;
 
 int main(int ac, char** argv)
 {
-    if (ac < 1)
+    if (ac <= 1)
     {
         cout << "*LOUD AND UNBEARABLE FEEDBACK NOISE*" << endl;
+    }
+    else 
+    {
+        int i;
+        int j;
+
+        for (i = 1; argv[i]; i++)
+        {
+            for (j = 0; argv[i][j]; j++)
+            {
+                cout <<  (char) toupper(argv[i][j]);
+            }
+            if (i < ac - 1)
+            {
+                cout << ' ';
+            }
+        }
+        cout << endl;
+        return(0);
     }
 }
---
+++ CPP00/ex01/PhoneBook.cpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   phonebook.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: etlim <etlim@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/15 18:25:45 by etlim             #+#    #+#             */
+/*   Updated: 2024/01/15 18:28:16 by etlim            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+class PhoneBook
+{
+private:
+    /* data */
+public:
+    
+PhoneBook(
+/* args */);
+    
+~PhoneBook(
+
+);
+};
+
+PhoneBook::PhoneBook(
+/* args */)
+{
+}
+
+PhoneBook::~PhoneBook(
+
+)
+{
+}
